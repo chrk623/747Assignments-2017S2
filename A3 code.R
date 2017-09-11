1b827efeb0f3cf14d577dd87fbc0c3186192151e
chrk623 <phui285@gmail.com>
2017-09-11 16:08:30 +1200

    deleted third model, added graphs

--- A3 code.R
+++ A3 code.R
@@ -5,6 +5,7 @@ original.df = read.csv("Assn3R.csv",header=T)
 str(original.df)
 #clean the data
 original.df$PV = as.numeric(gsub(",","",original.df$PV))
+original.df$Time = 1:(dim(original.df)[1])
 # original.df = select(original.df, -X)
 
 allcomb = function(x,y){
@@ -69,18 +70,34 @@ modelSim = function(data, decay){
   
   return(result)
 }
-
+#Create all possible combinations of decay rates for Rlive and Rock to 2dp
 Rock.decay = seq(0,1,by=0.01)
 Rlive.decay = seq(0,1,by=0.01)
 decays = allcomb(Rock.decay,Rlive.decay)
 
+#fitting the first model with original data
 original.result = modelSim(original.df, decays)
 original.result[[1]]
 pred = predict(original.result[[3]], original.result[[4]])
-pred = exp(pred)
+# pred = exp(pred)
 original.out = cbind(original.result[[4]], predicted = pred)
 # write.csv(original.out, "originalout.csv")
+plot(original.df$LN.PV., xlab = "Time", ylab = "Log(PV)", main = "Original Log(PV) 
+     vs Predicted Log(PV), Model1")
+lines(pred, col="blue")
+legend("topright", c("original","predicted"), lty=c(NA,1), pch=c(1,NA), col=c(1,"blue"))
 
+
+#model2 with chrismas variable
+#show weekly
+plot(original.df$LN.PV., type='l', main = "Log(PV) with weekly intervals", 
+     xlab = "Time", ylab = "Log(PV)")
+abline(v=seq(3, 116, by = 7), col = "red")
+polygon(original.df$Time[22:33],original.df$LN.PV[22:33], col = "green")
+points(original.df$Time[32],original.df$LN.PV[32], pch=19, col="blue", cex = 1.2)
+text(original.df$Time[32],original.df$LN.PV[32] - 0.1, "1/1/2008", font = 4, col = "blue")
+legend("topright", c("Log(PV)","Mondays", "Christmas period"), 
+       lty=c(1 ,1 ,NA), pch=c(NA,NA,15), col=c(1, "red", "green"))
 #22/12/2007 labour day, saturday
 # 25 26 christmas boxing
 #(01 02)/01/2008 new years
@@ -88,21 +105,20 @@ original.xmas = cbind(original.df,xmas=rep(0,dim(original.df)[1]))
 original.xmas$xmas[22:33]=1
 xmas.result = modelSim(original.xmas, decays)
 pred2 = predict(xmas.result[[3]], xmas.result[[4]])
-pred2 = exp(pred2)
+# pred2 = exp(pred2)
 original.xmas.out = cbind(xmas.result[[4]], predicted = pred2)
-# write.csv(original.xmas.out, "originalxmasout.csv")
+# write.csv(original.xmas.out, "ori ginalxmasout.csv")
+plot(original.df$LN.PV., xlab = "Time", ylab = "Log(PV)", main = "Original Log(PV) vs 
+     Predicted Log(PV), Model2")
+lines(pred2, col="blue")
+legend("topright", c("original","predicted"), lty=c(NA,1), pch=c(1,NA), col=c(1,"blue"))
+#assumption checks
+par(mfrow = c(2,2))
+plot(xmas.result[[3]], which = 1:6)
+par(mfrow = c(1,1)) 
+#model summary
+xmas.result[[1]]
+anova(xmas.result[[3]])
+xmas.result[[2]]
 
-noXmas = original.df[-(22:33),]
-noXmas.result = modelSim(noXmas, decays)
-diag = cbind((original.df[,-6])[-(22:33),], (noXmas.result[[4]])[,-1])
-pairs(diag)
-plot(noXmas.result[[3]], which=1:6)
-anova(noXmas.result[[3]])
-noXmas.result[[2]]
-noXmas.result[[1
-               ]]
-pred3 = predict(noXmas.result[[3]], noXmas.result[[4]])
-pred3 = exp(pred3)
-noXmas.out = cbind(noXmas.result[[4]], predicted = pred3)
-# write.csv(noXmas.out, "noXmasout.csv")
         
